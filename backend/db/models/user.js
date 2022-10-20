34f58a400646aafa5d9059f6a24287cc57878a71
ricalope <mrrobot421@gmail.com>
2022-10-20 14:23:49 -0700

    Add User model authentication Sequelize scopes and methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -2,10 +2,51 @@
 
 const { Model, Validator } = require('sequelize');
 
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
    class User extends Model {
+
+      toSafeObject() {
+         const { id, username, email } = this;
+         return { id, username, email };
+      }
+
+      validatePassword(password) {
+         return bcrypt.compareSync(password, this.hashedPassword.toString());
+      }
+
+      static getCurrentUserById(id) {
+         return User.scope('currentUser').findByPk(id);
+      }
+
+      static async login({ credential, password }) {
+         const { Op } = require('sequelize');
+         const user = await User.scope('loginUser').findOne({
+            where: {
+               [Op.or]: {
+                  username: credential,
+                  email: credential
+               }
+            }
+         });
+         if (user && user.validatePassword(password)) {
+            return await User.scope('currentUser').findByPk(user.id);
+         }
+      }
+
+      static async signup({ username, email, password }) {
+         const hashedPassword = bcrypt.hashSync(password);
+         const user = await User.create({
+            username,
+            email,
+            hashedPassword
+         });
+         return await User.scope('currentUser').findByPk(user.id)
+      }
+
       static associate(models) {
-         
+
       }
    }
    User.init({
@@ -39,6 +80,21 @@ module.exports = (sequelize, DataTypes) => {
    }, {
       sequelize,
       modelName: 'User',
+      defaultScope: {
+         attributes: {
+            exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+         }
+      },
+      scopes: {
+         currentUser: {
+            attributes: {
+               exclude: ['hashedPassword']
+            }
+         },
+         loginUser: {
+            attributes: {}
+         }
+      }
    });
    return User;
 };
