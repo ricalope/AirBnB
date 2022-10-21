aca9a5c66471d984e248b6fc758234011cdff58b
ricalope <mrrobot421@gmail.com>
2022-10-20 19:48:19 -0700

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -23,4 +23,9 @@ router.post('/', async (req, res, next) => {
    return res.json({ user });
 });
 
+router.delete('/', async (_req, res) => {
+   res.clearCookie('token');
+   return res.json({ message: 'success' });
+});
+
 module.exports = router;
