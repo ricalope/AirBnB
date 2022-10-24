3d146e6e81cfee8ecc21f335e9439ee099e30dc5
ricalope <mrrobot421@gmail.com>
2022-10-24 15:49:11 -0700

    added migration, model, and seed files for reviewimages

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
+const reviewImagesRouter = require('./review-images.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -12,6 +13,7 @@ router.use('/users', usersRouter);
 router.use('/spots', spotsRouter);
 router.use('/reviews', reviewsRouter);
 router.use('/bookings', bookingsRouter);
+router.use('/review-images', reviewImagesRouter);
 
 router.post('/test', (req, res) => {
    res.json({ requestBody: req.body });
