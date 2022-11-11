4968cbb16c7737be1402422a5c70fef62dbd1b6e
ricalope <mrrobot421@gmail.com>
2022-11-10 21:26:51 -0700

    added session action and reducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,47 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = user => ({
+   type: SET_USER,
+   payload: user
+})
+
+const removeUser = () => ({
+   type: REMOVE_USER
+})
+
+export const login = user => async dispatch => {
+   const { credential, password } = user;
+   const res = await csrfFetch('/api/session', {
+      method: 'POST',
+      body: JSON.stringify({
+         credential,
+         password
+      })
+   });
+   const data = await res.json();
+   dispatch(setUser(data));
+   return res
+}
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+   let newState;
+   switch(action.type) {
+      case SET_USER:
+         newState = Object.assign({}, state);
+         newState.user = action.payload;
+         return newState;
+      case REMOVE_USER:
+         newState = Object.assign({}, state);
+         newState.user = null;
+         return newState;
+      default:
+         return state;
+   }
+}
+
+export default sessionReducer
