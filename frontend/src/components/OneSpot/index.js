425a17cb2f49911b7812cae1ff9a5c51dcfe1ddc
ricalope <mrrobot421@gmail.com>
2022-11-20 17:24:34 -0700

    added css to spot details and added frontend readme

--- frontend/src/components/OneSpot/index.js
+++ frontend/src/components/OneSpot/index.js
@@ -25,39 +25,55 @@ function OneSpot() {
    if (!user?.id) {
       return isLoaded && (
          <div className="detail-container">
-         <div className="detail-top">
-            <div className="spot-name"><h2>{spot?.name}</h2></div>
-            <div className="detail-top-bottom">
-               <div className="spot-rating">
-                  {`★ ${spot?.avgStarRating ? Number(spot?.avgStarRating).toFixed(1) : ' No reviews for this hub yet.'}`}
-               </div>
-               <span>•</span>
-               <div>{`${reviews.length} reviews`}</div>
-               <span>•</span>
-               <div className="spot-location">
-                  {`${spot?.city}, ${spot?.state} ${spot?.country}`}
+            <div className="detail-top">
+               <div className="spot-name"><h2>{spot?.name}</h2></div>
+               <div className="detail-top-bottom">
+                  <div className="spot-rating">
+                     {`★ ${spot?.avgStarRating ? Number(spot?.avgStarRating).toFixed(1) : ' No reviews for this hub yet.'}`}
+                  </div>
+                  <span>•</span>
+                  <div>{`${reviews.length} reviews`}</div>
+                  <span>•</span>
+                  <div className="spot-location">
+                     {`${spot?.city}, ${spot?.state} ${spot?.country}`}
+                  </div>
                </div>
             </div>
-         </div>
-         <div className="detail-image">
-            <div className="one-image-div">
-               <img className="one-image" src={spot?.SpotImages[ 0 ]?.url} alt={spot?.name} />
+            <div className="detail-image">
+               <div className="one-image-div">
+                  <img className="one-image" src={spot?.SpotImages[ 0 ]?.url} alt={spot?.name} />
+               </div>
             </div>
-         </div>
-         <div className="bottom-container">
-            <div className="spot-owner">Hub hosted by {spot.Owner.firstName}</div>
-            <div className="spot-description">{spot.description}</div>
-            <div className="spot-reviews">
-               <ul>
+            <div className="bottom-container">
+               <div className="spot-owner">Hub hosted by {spot.Owner.firstName}</div>
+               <div className="spot-description">{spot.description}</div>
+               <div className="spot-reviews">
+               <div className="top-ratings">
+                  <div className="average-rating">
+                     {spot?.avgStarRating ? `★ ${Number(spot.avgStarRating).toFixed(1)}` : 'no reviews for this hub yet'}
+                  </div>
+                  <span>•</span>
+                  <div className="number-rating">
+                     {`${reviews?.length} reviews`}
+                  </div>
+               </div>
+               <div className="box-reviews">
                   {reviews.map(review => (
-                     <li key={review.id}>
-                        <Link to={`/spots/${spot.id}/reviews`}>
+                     <div className="user-reviews" key={review.id}>
+                        <div className="review-top">
+                           <div className="review-icon">
+                              <i className="fa-solid fa-user" />
+                           </div>
+                           <div className="prof-content">
+                              <div className="firstname">{review?.User?.firstName}</div>
+                           </div>
+                        </div>
+                        <div className="link-review"><Link to={`/spots/${spot.id}/reviews`}>
                            {review?.review}
-                        </Link>
-                        <p>{`- ${review?.User.firstName} ${review?.stars} ★`}</p>
-                     </li>
+                        </Link></div>
+                     </div>
                   ))}
-               </ul>
+               </div>
             </div>
             </div>
          </div>
@@ -80,6 +96,12 @@ function OneSpot() {
                <div className="spot-location">
                   {`${spot?.city}, ${spot?.state} ${spot?.country}`}
                </div>
+               {user?.id === spot?.ownerId && (
+                  <div className="edit-delete-links">
+                     <p id="edit"><Link to={`/spots/${spot?.id}/edit`}>edit hub</Link></p>
+                     <p id="del"><Link to={`/spots/${spot?.id}/delete`}>delete hub</Link></p>
+                  </div>
+               )}
             </div>
          </div>
          <div className="detail-image">
@@ -90,31 +112,41 @@ function OneSpot() {
          <div className="bottom-container">
             <div className="spot-owner">Hub hosted by {spot.Owner.firstName}</div>
             <div className="spot-description">{spot.description}</div>
-            {user?.id === spot?.ownerId && (
-               <div>
-                  <Link to={`/spots/${spot?.id}/edit`}>edit hub</Link>
-                  <Link to={`/spots/${spot?.id}/delete`}>delete hub</Link>
-               </div>
-            )}
             {user?.id !== spot?.ownerId && !existingReview && (
                <div className="add-review">
                   <Link to={`/spots/${spot?.id}/reviews/new`}>Leave a review</Link>
                </div>
             )}
             <div className="spot-reviews">
-               <ul>
+               <div className="top-ratings">
+                  <div className="average-rating">
+                     {spot?.avgStarRating ? `★ ${Number(spot.avgStarRating).toFixed(1)}` : 'no reviews for this hub yet'}
+                  </div>
+                  <span>•</span>
+                  <div className="number-rating">
+                     {`${reviews?.length} reviews`}
+                  </div>
+               </div>
+               <div className="box-reviews">
                   {reviews.map(review => (
-                     <li key={review.id}>
-                        <Link to={`/spots/${spot.id}/reviews`}>
+                     <div className="user-reviews" key={review.id}>
+                        <div className="review-top">
+                           <div className="review-icon">
+                              <i className="fa-solid fa-user" />
+                           </div>
+                           <div className="prof-content">
+                              <div className="firstname">{review?.User?.firstName}</div>
+                              <div className="remove"><p>{user.id === review.userId && (
+                                 <Link to={`/reviews/${review.id}/delete`}>delete review</Link>
+                              )}</p></div>
+                           </div>
+                        </div>
+                        <div className="link-review"><Link to={`/spots/${spot.id}/reviews`}>
                            {review?.review}
-                        </Link>
-                        <p>{`- ${review?.User.firstName} ${review?.stars} ★`}</p>
-                        <p>{user.id === review.userId && (
-                              <Link to={`/reviews/${review.id}/delete`}>delete</Link>
-                           )}</p>
-                     </li>
+                        </Link></div>
+                     </div>
                   ))}
-               </ul>
+               </div>
             </div>
          </div>
       </div>
